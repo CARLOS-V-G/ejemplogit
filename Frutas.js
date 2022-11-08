7174299ecdbfc00cd4b695600469913087c34e25
CARLOS-V-G <cv5944341@gmail.com>
2022-11-08 09:33:55 -0300

    agrego mas frutas

--- Frutas.js
+++ Frutas.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-const frutas = ['Tomate', 'Naranja','Pera','manzana','durazno','frutilla']
-=======
-const frutas = ['Tomate', 'Naranja','Pera','manzana','durazno']
 
-module.exports = frutas;
->>>>>>> 7c95c4d1db9297a49ba05674a8ed35168fca0ef2
+const frutas = ['Tomate', 'Naranja','Pera','manzana','durazno','frutilla']
